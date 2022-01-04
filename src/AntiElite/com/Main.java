98cb02eb5c6148a9cf2abe82e369d5b015a4f2ae
Nametsego <Nametsegothakadu@gmail.com>
2022-01-04 05:52:53 +0200

    Initial Commit

---
+++ src/AntiElite/com/Main.java
@@ -0,0 +1,10 @@
+package AntiElite.com;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"You really dont code in Java?","Question",JOptionPane.QUESTION_MESSAGE);
+    }
+}
